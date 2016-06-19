8f4a05bc3ac57b2506fd3a7d2a4aa7544b4a1308
Oscar Utbult <oscar@oscr.io>
2016-06-19 15:28:21 +0200

    Add example

---
+++ src/main/java/generics/Example_List_Erasure_Function.java
@@ -0,0 +1,22 @@
+package generics;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Example_List_Erasure_Function {
+    public static void main(String[] args) {
+        // Remove commented out functions and note
+        // compile error due functions having same erasure!
+    }
+
+    /*
+    private void max(List<Integer> i) {
+
+    }
+
+    private void max(List<String> s) {
+
+    }
+    */
+}
