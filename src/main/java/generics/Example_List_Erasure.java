cb0c6634b53020bc606463474bc0df0ff1174c24
Oscar Utbult <oscar@oscr.io>
2016-06-19 13:28:17 +0200

    Add example

---
+++ src/main/java/generics/Example_List_Erasure.java
@@ -0,0 +1,16 @@
+package generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example_List_Erasure {
+    public static void main(String[] args) {
+        List<Apple> apples = new ArrayList<>();
+        List<Candy> candies = new ArrayList<>();
+
+        System.out.println(apples.getClass() == candies.getClass());
+    }
+    
+    class Apple {}
+    class Candy {}
+}
