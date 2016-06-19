07594b9ec1dc96178df7d703c0e08f3f73a87f84
Oscar Utbult <oscar@oscr.io>
2016-06-19 15:33:26 +0200

    Rename file

--- src/main/java/generics/Example_List_Invariant.java
+++ src/main/java/generics/Example_List_Invariant.java
@@ -3,7 +3,7 @@ package generics;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Example_List {
+public class Example_List_Invariant {
     public static void main(String[] args) {
         List<Integer> ints = new ArrayList<>();
 
